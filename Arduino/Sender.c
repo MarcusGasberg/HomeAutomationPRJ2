b57a72c16addb323d5271bb1fd0c6c62796c0235
Valdemartt <36989028+Valdemartt@users.noreply.github.com>
2018-04-11 15:08:40 +0200

    Drivers til sender
    Påbegyndt drivers til sender

---
+++ Arduino/Sender.c
@@ -0,0 +1,32 @@
+/*
+ * Sender.c
+ *
+ * Created: 11-04-2018 14:03:47
+ *  Author: valde
+ */ 
+
+
+#include <avr/io.h>
+#include "Sender.h"
+#define F_CPU 16000000
+#include <util/delay.h>
+#include <avr/interrupt.h>
+
+
+void sendPWM(){
+	TCCR1A |= 0b10000011;
+	TCCR1B |= 0b00011001;
+	OCR1A = 132/2;
+	ICR1 = 132 
+	DDRB |= 0b00100000;
+	_delay_ms(1);
+	DDRB = 0;
+	TCCR1A = 0;
+	TCCR1B = 0;
+	OCR1A = 0;
+	ICR1 = 0;
+}
+
+void sendCommand(char * address, char command){
+	
+}
---
+++ Arduino/Sender.h
@@ -0,0 +1,21 @@
+/*
+ * Sender.h
+ *
+ * Created: 11-04-2018 14:04:09
+ *  Author: valde
+ */ 
+
+
+#ifndef SENDER_H_
+#include <string.h>
+#define SENDER_H_
+
+void initSender();
+void sendPWM();
+void sendCommand(char *,char);
+
+
+
+
+
+#endif /* SENDER_H_ */
